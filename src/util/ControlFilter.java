1d2fbf530d949b159ba62476bf18a989ee087110
junioma <junioma@1fa0c2c6-3123-11de-9cad-a3574e5575ef>
2009-05-06 22:13:24 +0000

    Merge

--- src/util/ControlFilter.java
+++ src/util/ControlFilter.java
@@ -15,25 +15,17 @@ public class ControlFilter {
 		List<Evento> listEventos = new ArrayList<Evento>();
 		Directory diretorio = new Directory("C:/Lucene/JavaDotNet/data");//Directory used by data 
 		diretorio.loadDataFromDirectory();//Read the data from directory, only the files descriptors
-		//ArrayList<String>arrayListDeEventos = new ArrayList<String>();
-        //Parse parse = new Parse("\\[[0-9][0-9][0-9][0-9]\\-[0-9][0-9]\\-[0-9][0-9] [0-9][0-9]\\:[0-9][0-9]\\:[0-9][0-9]\\]\\[[0-9][0-9][0-9]\\] \\[[A-Z]*[a-z]*\\] \\[.*\\] \\[.*\\]");
         Parse parse = new Parse();
 		parse.addOrderOfParse(Evento.EVENTO.DATAHORA);
         parse.addOrderOfParse(Evento.EVENTO.ID);
         parse.addOrderOfParse(Evento.EVENTO.NIVEL);
         parse.addOrderOfParse(Evento.EVENTO.CLASSE);
         parse.addOrderOfParse(Evento.EVENTO.MENSAGEM); 
-        /*parse.setDataHoraParse("[0-9][0-9][0-9][0-9]\\-[0-9][0-9]\\-[0-9][0-9] [0-9][0-9]\\:[0-9][0-9]\\:[0-9][0-9]");
-        parse.setIdParse("[0-9][0-9][0-9]");
-        parse.setNivelParse("[A-Z]*[a-z]*");
-        parse.setClasseParse("\\[.*\\]");
-        parse.setMensagemParse(".*");*/
-        System.out.println("Quantidade total de regitros:"+SequencialReader.countEventsByParse(diretorio.getDirectoryFiles(), parse,filtro));
+        System.out.println("google:Quantidade total de regitros:"+SequencialReader.countEventsByParse(diretorio.getDirectoryFiles(), parse,filtro));
         //SequencialReader.readEventsAll(diretorio.getDirectoryFiles(), arrayListDeEventos,parse,500,600);
 		//SequencialReader.readEventsCheckingByFilter(diretorio.getDirectoryFiles(), arrayListDeEventos, parse, filtro, 2, 5);
         SequencialReader.readEventsCheckingByFilter(diretorio.getDirectoryFiles(), listEventos, parse, filtro, 1, SequencialReader.countEventsByParse(diretorio.getDirectoryFiles(), parse,filtro));
-        //CastEvento.manyFromString(arrayListDeEventos, listEventos,parse);
-		//arrayListDeEventos = null;
+        //CastEvento.manyFromString(arrayListDeEventos, listEventos,parse);;
 		return listEventos;
 	}
 	
