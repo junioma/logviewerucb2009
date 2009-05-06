dc2a78328a2b01a964996d3eb2f67469b915b142
junioma <junioma@1fa0c2c6-3123-11de-9cad-a3574e5575ef>
2009-05-06 22:12:51 +0000

    Merge

---
+++ trunk/src/util/Constantes.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * @author matheus
+ *
+ */
+public class Constantes {
+	public static Integer NIVEL_ERROR = new Integer(5);
+	public static Integer NIVEL_WARNING = new Integer(4);
+	public static Integer NIVEL_FINE = new Integer(3);
+	public static Integer NIVEL_INFO = new Integer(2);
+	public static Integer NIVEL_DEBUG = new Integer(1);
+
+	public static String CAMINHO_IMAGEM_NIVEL = "/Web/imagens/nivel/";
+	
+	public static String IMAGEM_ERROR = CAMINHO_IMAGEM_NIVEL+"nivel-error.gif";
+	public static String IMAGEM_WARNING = CAMINHO_IMAGEM_NIVEL+"nivel-warning.gif";
+	public static String IMAGEM_FINE = CAMINHO_IMAGEM_NIVEL+"nivel-fine.gif";
+	public static String IMAGEM_INFO = CAMINHO_IMAGEM_NIVEL+"nivel-info.gif";
+	public static String IMAGEM_DEBUG = CAMINHO_IMAGEM_NIVEL+"nivel-debug.gif";
+	
+
+}
