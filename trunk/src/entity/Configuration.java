c388e72b70fb5717d59873da057d4d28f62440fd
junioma <junioma@1fa0c2c6-3123-11de-9cad-a3574e5575ef>
2009-05-11 01:16:34 +0000

    -Criado a classe de configuração em padrão singleton -Alterado o funcionamento do método "recursiveDirectoryWalker" para chegar as extensões de arquivos listados na classe de coniguração -Incorporado a classe de configuração na ControlFilter

---
+++ trunk/src/entity/Configuration.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**This Class hold the basic configuration of the application.
+
+ * @author Bruno / Junio / Matheus
+
+ * @version 1.0
+
+ * @since 1.0
+
+ */
+public class Configuration {
+	private static Configuration uniqueInstance;
+	private String logsPath;
+	private String log4JPatternLayoutPath;
+	private List<String> fileExtension;
+	private Configuration(){
+		logsPath = null;
+		log4JPatternLayoutPath = null;
+		fileExtension = new ArrayList<String>();
+	}
+	public String getLogsPath() {
+		return logsPath;
+	}
+	public void setLogsPath(String logsPath) {
+		this.logsPath = logsPath;
+		logsPath = null;
+	}
+	public String getLog4JPatternLayoutPath() {
+		return log4JPatternLayoutPath;
+	}
+	public void setLog4JPatternLayoutPath(String log4JPatternLayoutPath) {
+		this.log4JPatternLayoutPath = log4JPatternLayoutPath;
+		log4JPatternLayoutPath = null;
+	}
+	
+	public List<String> getFileExtension() {
+		return fileExtension;
+	}
+	public void setFileExtension(List<String> fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+	public int addFileExtension(String fileExtension){
+		int indexAdded=-1;
+		if(this.fileExtension!=null && fileExtension!=null){
+			if(this.fileExtension.add(fileExtension)){
+				indexAdded=fileExtension.indexOf(fileExtension);
+			}
+			else{
+				indexAdded=-2;
+			}
+		}
+		else{
+			return -1;
+		}
+		return indexAdded;
+	}
+	
+	public static Configuration getInstance(){
+		if (uniqueInstance == null){
+			uniqueInstance = new Configuration();
+		}
+		return uniqueInstance;
+	}
+	
+}
