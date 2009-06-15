47c2c4cf63c6abbb8a73ee66cbbc20783967f456
junioma <junioma@1fa0c2c6-3123-11de-9cad-a3574e5575ef>
2009-06-15 03:57:53 +0000

    Adicionado o texto da GPL.

--- src/logview/java/dao/reader/CastEvento.java
+++ src/logview/java/dao/reader/CastEvento.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2003-2008 Universidade Católica de Brasília, Brasil
+ *
+ * Este arquivo é parte do Easy2C.
+ *
+ * O Easy2C é um software livre; você pode redistribui-lo e/ou modificá-lo
+ * dentro dos termos da Licença Pública Geral GNU versão 2 como publicada
+ * pela Fundação do Software Livre (FSF).
+ *
+ * Este programa é distribuido na esperança que possa ser util, mas SEM
+ * NENHUMA GARANTIA; sem uma garantia implicita de ADEQUAÇÂO a qualquer
+ * MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU
+ * para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o
+ * título "Licenca.txt", junto com este programa, se não, escreva para a
+ * Fundação do Software Livre(FSF) Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 package logview.java.dao.reader;
 
 import java.util.ArrayList;
